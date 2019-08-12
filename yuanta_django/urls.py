88655b1fa42753a3344b7098f7c22ce008a6be9b
vincenttuan <vincentjava@yahoo.com.tw>
2019-08-12 14:26:07 +0800

    django

--- yuanta_django/urls.py
+++ yuanta_django/urls.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.urls import path
 
 
 def hello(request):
     return HttpResponse("Hello Django")
 
+
+def test(request):
+    return render(request, 'test.html')
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', hello),
+
+    path('test/', test)
 ]
